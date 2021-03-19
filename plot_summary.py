8bae5f25e51fb7befefd632eaf38b173fb0793be
Einar <einar.ameen@dsa.no>
2021-03-19 19:44:02 +0100

    Adds countermeasure rings

--- plot_summary.py
+++ plot_summary.py
@@ -21,45 +21,51 @@ Use 'max' or 'mean' on full input.
 DOSE_CRITERIA = {
     'iodine_child': 10,  # mGy end pf run
     'iodine_adult': 50,  # mGy end pf run
-    'evac': 20, # 7 dager
-    'sheltering_partial': 1, # mSv 2 dager
+    'evac': 20,  # 7 dager
+    'sheltering_partial': 1,  # mSv 2 dager
     'sheltering_full': 10,   # mSv 2 dager
-        }
-DOSE_CRITERIA_SI = {k:v/1000 for k,v in DOSE_CRITERIA.items() }
+}
+DOSE_CRITERIA_SI = {k: v/1000 for k, v in DOSE_CRITERIA.items()}
 
 # %%
-def dump_full_shp(run,df):
+
+
+def dump_full_shp(run, df):
     gdf = gpd.GeoDataFrame(df)
     gdf['geometry'] = gdf['geom_str'].apply(wkb.loads)
-    gdf = gdf[['Value','geometry']]
+    gdf = gdf[['Value', 'geometry']]
     gdf.to_file(f"{run}_full_shppackage.gpkg", layer='evac', driver="GPKG")
 
+
 def read_df(run):
-    df=pd.read_pickle(run)
+    df = pd.read_pickle(run)
     # dump_full_shp(run, df)
-    df=df.groupby('geom_str').agg({'Value': 'max'})
+    df = df.groupby('geom_str').agg({'Value': 'max'})
     df.reset_index(inplace=True)
 
     gdf = gpd.GeoDataFrame(df)
     gdf['geometry'] = gdf['geom_str'].apply(wkb.loads)
-    gdf = gdf[['Value','geometry']]
+    gdf = gdf[['Value', 'geometry']]
 
     # gdf.to_file(f"{run}_shp")
 
     return gdf
 # %%
+
+
 def plot(run, criteria=''):
     # %%
     gdf = read_df(run)
     # %%
-    run = run.replace(".pkl.bz2",'_')
+    run = run.replace(".pkl.bz2", '_')
     fig, ax = plt.subplots(dpi=250)  # figsize=(10, 10))
 
-    gdf_out = gdf[gdf.Value > 0] # for speedup
+    gdf_out = gdf[gdf.Value > 0]  # for speedup
     if criteria:
-        gdf_out.Value = (gdf_out.Value > DOSE_CRITERIA_SI.get(criteria)).astype(int)
+        gdf_out.Value = (
+            gdf_out.Value > DOSE_CRITERIA_SI.get(criteria)).astype(int)
 
-        # New zeros whrn criteria applied: 
+        # New zeros whrn criteria applied:
         gdf_out = gdf_out[gdf_out.Value > 0]
     gdf_out = gdf_out.set_crs('EPSG:4326')
 
@@ -67,34 +73,36 @@ def plot(run, criteria=''):
 
     gdf_out.plot(ax=ax, edgecolor="face", linewidth=1)
 
-
     minx, miny, maxx, maxy = gdf_out.geometry.total_bounds
     padding_m = (maxx-minx)/100*100*.66
     ax.set_xlim(minx - padding_m, maxx + padding_m)
     ax.set_ylim(miny - padding_m, maxy + padding_m)
 
-    #Add scale-bar
-    x, y, scale_len = maxx+padding_m-2500, miny-padding_m+200, 2000 #arrowstyle='-'
-    scale_rect = matplotlib.patches.Rectangle((x,y),scale_len,100,linewidth=0.5,edgecolor='k',facecolor='k')
+    # Add scale-bar
+    x, y, scale_len = maxx+padding_m-2500, miny - \
+        padding_m+200, 2000  # arrowstyle='-'
+    scale_rect = matplotlib.patches.Rectangle(
+        (x, y), scale_len, 100, linewidth=0.5, edgecolor='k', facecolor='k')
     ax.add_patch(scale_rect)
-    plt.text(x+scale_len/2, y+300, s='2 KM', fontsize=6, horizontalalignment='center')
+    plt.text(x+scale_len/2, y+300, s='2 KM',
+             fontsize=6, horizontalalignment='center')
 
     # Add release point
     releasepoint = overlap.create_point(overlap.GROTSUND_COORD)
     releasepoint.plot(ax=ax, color='k', marker='x', markersize=150)
     # plt.plot([overlap.GROTSUND_COORD[0]], [overlap.GROTSUND_COORD[1]], '+', color='black', markersize=15)
 
-
     basemap = cx.providers.CartoDB.Voyager
     #basemap = cx.providers.Stamen.Terrain
     cx.add_basemap(ax, crs="EPSG:25833", source=basemap,
-                attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
+                   attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
 
     # ax.set_title('title')
     ax.set_xlabel('')
     ax.set_ylabel('')
     ax.margins(0)
-    ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+    ax.tick_params(left=False, labelleft=False,
+                   bottom=False, labelbottom=False)
 
     # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0)
 
@@ -102,12 +110,14 @@ def plot(run, criteria=''):
     ax.axes.yaxis.set_visible(False)
     plt.tight_layout()
     fig.tight_layout()
-    fig.canvas.start_event_loop(sys.float_info.min) #workaround for Exception in Tkinter callback
+    # workaround for Exception in Tkinter callback
+    fig.canvas.start_event_loop(sys.float_info.min)
 
     fig.savefig(f"{run}{criteria}.png", bbox_inches='tight')
     print(f'Saved {run}{criteria}.png')
 # %%
 # %%
+
 
 def notebook_dummy():
     """Run below cell and then cells inside plot()
@@ -118,12 +128,13 @@ def notebook_dummy():
     criteria = 'evac'
 
     # %%
+
+
 def create_all(base):
     # %%
     # Full sheltering
     run = f'{base}4800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
     plot(run, 'sheltering_full')
-
 
     # Partial sheltering
     run = f'{base}4800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
@@ -138,7 +149,7 @@ def create_all(base):
     plot(run, 'iodine_adult')
 
     # Evac adult
-    run =f'{base}16800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
+    run = f'{base}16800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
     plot(run, 'evac')
 
 
@@ -147,7 +158,7 @@ def plot_run_boundary65km():
     # %%
     from shapely.geometry import Polygon, mapping
     run = "run_boundary65km"
-    ring = overlap.isocurve(65,overlap.GROTSUND_COORD)
+    ring = overlap.isocurve(65, overlap.GROTSUND_COORD)
     fig, ax = plt.subplots(dpi=250)  # figsize=(10, 10))
 
     ring = ring.set_crs('EPSG:4326')
@@ -159,29 +170,35 @@ def plot_run_boundary65km():
     padding_m = (maxx-minx)/100*100*.13
     ax.set_xlim(minx - padding_m, maxx + padding_m)
     ax.set_ylim(miny - padding_m, maxy + padding_m)
-    
+
     # #Add scale-bar
-    x, y, scale_len = maxx+padding_m-25000, miny-padding_m+1200, 20000 #arrowstyle='-'
-    scale_rect = matplotlib.patches.Rectangle((x,y),scale_len,100,linewidth=0.5,edgecolor='k',facecolor='k')
+    x, y, scale_len = maxx+padding_m-25000, miny - \
+        padding_m+1200, 20000  # arrowstyle='-'
+    scale_rect = matplotlib.patches.Rectangle(
+        (x, y), scale_len, 100, linewidth=0.5, edgecolor='k', facecolor='k')
     ax.add_patch(scale_rect)
-    plt.text(x+scale_len/2, y+1300, s='20 KM', fontsize=6, horizontalalignment='center')
+    plt.text(x+scale_len/2, y+1300, s='20 KM',
+             fontsize=6, horizontalalignment='center')
 
     # Add release point
 
-    ring.boundary.plot(ax=ax, edgecolor='black')#, color=None, edgecolor='k',linewidth = 2, )#facecolor=None)
-    releasepoint= overlap.create_point(overlap.GROTSUND_COORD)
-    releasepoint.plot(ax=ax,  marker='x', color='black')#, markersize=150, color=None, edgecolor='k',linewidth = 2, facecolor=None)
+    # , color=None, edgecolor='k',linewidth = 2, )#facecolor=None)
+    ring.boundary.plot(ax=ax, edgecolor='black')
+    releasepoint = overlap.create_point(overlap.GROTSUND_COORD)
+    # , markersize=150, color=None, edgecolor='k',linewidth = 2, facecolor=None)
+    releasepoint.plot(ax=ax,  marker='x', color='black')
 
     basemap = cx.providers.CartoDB.Voyager
     #basemap = cx.providers.Stamen.Terrain
     cx.add_basemap(ax, crs="EPSG:25833", source=basemap,
-                attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
+                   attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
 
     # ax.set_title('title')
     ax.set_xlabel('')
     ax.set_ylabel('')
     ax.margins(0)
-    ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+    ax.tick_params(left=False, labelleft=False,
+                   bottom=False, labelbottom=False)
 
     # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0)
 
@@ -189,17 +206,123 @@ def plot_run_boundary65km():
     ax.axes.yaxis.set_visible(False)
     plt.tight_layout()
     fig.tight_layout()
-    fig.canvas.start_event_loop(sys.float_info.min) #workaround for Exception in Tkinter callback
+    # workaround for Exception in Tkinter callback
+    fig.canvas.start_event_loop(sys.float_info.min)
 
     fig.savefig(f"{run}.png", bbox_inches='tight')
     print(f'Saved {run}.png')
     # %%
 
+
+def get_ring(countermeasure, level='maks'):
+    from shapely.geometry import Polygon, mapping
+    distances = {
+        "Matproduksjon": {
+            "maks": 55.0,
+            "95-persentil": 55.0
+        },
+        "Innemelding": {
+            "maks": 2.5,
+            "95-persentil": 0.8
+        },
+        "Delvis_innemelding": {
+            "maks": 5.0,
+            "95-persentil": 4.0
+        },
+        "Evakuering (akutt)": {
+            "maks": 1.75,
+            "95-persentil": 0.5
+        },
+        "Jod_barn": {
+            "maks": 9.0,
+            "95-persentil": 4.0
+        },
+        "Jod_voksne": {
+            "maks": 3.0,
+            "95-persentil": 1.5
+        }
+    }
+    ring = overlap.isocurve(
+        distances[countermeasure][level], overlap.GROTSUND_COORD)
+
+    ring = ring.set_crs('EPSG:4326')
+
+    ring.to_crs('EPSG:25833', inplace=True)
+    ring.geometry = [Polygon(mapping(x)['coordinates']) for x in ring.geometry]
+
+    return ring
+
+
+def plot_CM(countermeasure):
+
+    # %%
+    run = "tiltak_"+countermeasure
+    fig, ax = plt.subplots(dpi=100)  # figsize=(10, 10))
+
+    ring = get_ring(countermeasure, 'maks')
+    ring.boundary.plot(ax=ax, edgecolor='black', linestyle=':',)
+
+    ring = get_ring(countermeasure, '95-persentil')
+    ring.boundary.plot(ax=ax, edgecolor='black', linestyle='-',)
+
+    ring = get_ring(countermeasure)
+    ring.boundary.plot(ax=ax, edgecolor='black', linestyle=':',)
+
+    minx, miny, maxx, maxy = ring.geometry.total_bounds
+    padding_m = (maxx-minx)/100*100*.15
+    ax.set_xlim(minx - padding_m, maxx + padding_m)
+    ax.set_ylim(miny - padding_m, maxy + padding_m)
+
+    # Add scale-bar
+    x, y, scale_len = maxx+padding_m-2500, miny - \
+        padding_m+200, 2000  # arrowstyle='-'
+    scale_rect = matplotlib.patches.Arrow(
+        x, y, scale_len, 0, linewidth=1, edgecolor='k', facecolor='k')
+    ax.add_patch(scale_rect)
+    plt.text(x+scale_len/2, y+100, s='2 KM',
+            horizontalalignment='center') # fontsize=6,
+
+    # Add release point
+    releasepoint = overlap.create_point(overlap.GROTSUND_COORD)
+    releasepoint.plot(ax=ax,  marker='x', color='black')
+
+    basemap = cx.providers.CartoDB.Voyager
+    #basemap = cx.providers.Stamen.Terrain
+    graatone = {'url': "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}",
+                'attribution': '© Kartverket'}
+    grunnkart = {'url': "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}",
+                 'attribution': '© Kartverket'}
+
+    basemap = grunnkart
+    attrib = f"Bakgrunnskart: {basemap['attribution']}"
+    # cx.add_basemap(ax, crs="EPSG:25833", source="https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}",
+    cx.add_basemap(ax, crs="EPSG:25833", source=basemap, attribution=attrib,) #attribution_size=6)  # , zoom=15)
+
+    # ax.set_title('title')
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.margins(0)
+    ax.tick_params(left=False, labelleft=False,
+                   bottom=False, labelbottom=False)
+
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    plt.tight_layout()
+    fig.tight_layout()
+
+    # workaround for Exception in Tkinter callback:
+    fig.canvas.start_event_loop(sys.float_info.min)
+
+    fig.savefig(f"{run}.png", bbox_inches='tight')
+    print(f'Saved {run}.png')
+    # %%
+
+
 def plot_run_boundary5km():
     # %%
     from shapely.geometry import Polygon, mapping
     run = "run_boundary5km"
-    ring = overlap.isocurve(5,overlap.GROTSUND_COORD)
+    ring = overlap.isocurve(5, overlap.GROTSUND_COORD)
     fig, ax = plt.subplots(dpi=250)  # figsize=(10, 10))
 
     ring = ring.set_crs('EPSG:4326')
@@ -212,28 +335,34 @@ def plot_run_boundary5km():
     ax.set_xlim(minx - padding_m, maxx + padding_m)
     ax.set_ylim(miny - padding_m, maxy + padding_m)
 
-    #Add scale-bar
-    x, y, scale_len = maxx+padding_m-2500, miny-padding_m+200, 2000 #arrowstyle='-'
-    scale_rect = matplotlib.patches.Rectangle((x,y),scale_len,100,linewidth=0.5,edgecolor='k',facecolor='k')
+    # Add scale-bar
+    x, y, scale_len = maxx+padding_m-2500, miny - \
+        padding_m+200, 2000  # arrowstyle='-'
+    scale_rect = matplotlib.patches.Rectangle(
+        (x, y), scale_len, 100, linewidth=0.5, edgecolor='k', facecolor='k')
     ax.add_patch(scale_rect)
-    plt.text(x+scale_len/2, y+300, s='2 KM', fontsize=6, horizontalalignment='center')
+    plt.text(x+scale_len/2, y+300, s='2 KM',
+             fontsize=6, horizontalalignment='center')
 
     # Add release point
 
-    ring.boundary.plot(ax=ax, edgecolor='black')#, color=None, edgecolor='k',linewidth = 2, )#facecolor=None)
-    releasepoint= overlap.create_point(overlap.GROTSUND_COORD)
-    releasepoint.plot(ax=ax,  marker='x', color='black')#, markersize=150, color=None, edgecolor='k',linewidth = 2, facecolor=None)
+    # , color=None, edgecolor='k',linewidth = 2, )#facecolor=None)
+    ring.boundary.plot(ax=ax, edgecolor='black')
+    releasepoint = overlap.create_point(overlap.GROTSUND_COORD)
+    # , markersize=150, color=None, edgecolor='k',linewidth = 2, facecolor=None)
+    releasepoint.plot(ax=ax,  marker='x', color='black')
 
     basemap = cx.providers.CartoDB.Voyager
     #basemap = cx.providers.Stamen.Terrain
     cx.add_basemap(ax, crs="EPSG:25833", source=basemap,
-                attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
+                   attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
 
     # ax.set_title('title')
     ax.set_xlabel('')
     ax.set_ylabel('')
     ax.margins(0)
-    ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+    ax.tick_params(left=False, labelleft=False,
+                   bottom=False, labelbottom=False)
 
     # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0)
 
@@ -241,11 +370,13 @@ def plot_run_boundary5km():
     ax.axes.yaxis.set_visible(False)
     plt.tight_layout()
     fig.tight_layout()
-    fig.canvas.start_event_loop(sys.float_info.min) #workaround for Exception in Tkinter callback
+    # workaround for Exception in Tkinter callback
+    fig.canvas.start_event_loop(sys.float_info.min)
 
     fig.savefig(f"{run}.png", bbox_inches='tight')
     print(f'Saved {run}.png')
     # %%
+
 
     # %%
 if __name__ == '__main__':
@@ -256,6 +387,7 @@ if __name__ == '__main__':
     # plot_run_boundary5km()
     # plot_run_boundary65km()
 
-   
     # create_all(base5km)
     # create_all(base60km)
+
+    plot_CM('Innemelding')

3666878da9cae3ee208b88a064464bdbcbee2590
Einar <einar.ameen@dsa.no>
2021-03-26 11:29:30 +0100

    Last working all run summary plot

--- plot_summary.py
+++ plot_summary.py
@@ -24,6 +24,8 @@ DOSE_CRITERIA = {
     'evac': 20,  # 7 dager
     'sheltering_partial': 1,  # mSv 2 dager
     'sheltering_full': 10,   # mSv 2 dager
+    #Other:
+    'reinsdyr': 2000*1000, # kBq/m2 depo. Workaround for SI: *1000 
 }
 DOSE_CRITERIA_SI = {k: v/1000 for k, v in DOSE_CRITERIA.items()}
 
@@ -78,28 +80,43 @@ def plot(run, criteria=''):
     run = run.replace(".pkl.bz2", '_')
 
     minx, miny, maxx, maxy = gdf_out.geometry.total_bounds
-    padding_m = (maxx-minx)/100*100*.66
+    padding_m = (maxx-minx)/100*100*.15
     ax.set_xlim(minx - padding_m, maxx + padding_m)
     ax.set_ylim(miny - padding_m, maxy + padding_m)
 
     # Add scale-bar
-    x, y, scale_len = maxx+padding_m-2500, miny - \
-        padding_m+200, 2000  # arrowstyle='-'
-    scale_rect = matplotlib.patches.Rectangle(
-        (x, y), scale_len, 100, linewidth=0.5, edgecolor='k', facecolor='k')
-    ax.add_patch(scale_rect)
-    plt.text(x+scale_len/2, y+300, s='2 KM',
-             fontsize=6, horizontalalignment='center')
+    if maxx-minx < 15000:
+        scale_m = 2000
+    elif maxx-minx < 40000:
+        scale_m = 5000
+    else:
+        scale_m = 20000
+
+    x = maxx + padding_m - (maxx - minx)*0.015 - scale_m
+    y = miny - padding_m + (maxy - miny)*.015
+
+    # y  =  miny*0.9999999460
+    scale_line = matplotlib.patches.Arrow(
+        x, y, scale_m, 0, linewidth=1, edgecolor='k', facecolor='k')
+    ax.add_patch(scale_line)
+    plt.text(x+scale_m/2, y+(maxy - miny)*.01, s=f'{scale_m/1000:.0f} Km',
+             horizontalalignment='center')  # fontsize=6,
 
     # Add release point
     releasepoint = overlap.create_point(overlap.GROTSUND_COORD)
-    releasepoint.plot(ax=ax, color='k', marker='x', markersize=150)
-    # plt.plot([overlap.GROTSUND_COORD[0]], [overlap.GROTSUND_COORD[1]], '+', color='black', markersize=15)
+    releasepoint.plot(ax=ax,  marker='x', color='black')
 
-    basemap = cx.providers.CartoDB.Voyager
+    # basemap = cx.providers.CartoDB.Voyager
     #basemap = cx.providers.Stamen.Terrain
-    cx.add_basemap(ax, crs="EPSG:25833", source=basemap,
-                   attribution=f"Bakgrunnskart: {basemap.attribution}", attribution_size=6)
+    graatone = {'url': "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}",
+                'attribution': '© Kartverket'}
+    grunnkart = {'url': "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}",
+                 'attribution': '© Kartverket'}
+
+    basemap = grunnkart
+    attrib = f"Bakgrunnskart: {basemap['attribution']}"
+    # attribution_size=6)  # , zoom=15)
+    cx.add_basemap(ax, crs="EPSG:25833", source=basemap, attribution=attrib,)
 
     # ax.set_title('title')
     ax.set_xlabel('')
@@ -108,13 +125,12 @@ def plot(run, criteria=''):
     ax.tick_params(left=False, labelleft=False,
                    bottom=False, labelbottom=False)
 
-    # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0)
-
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
     plt.tight_layout()
     fig.tight_layout()
-    # workaround for Exception in Tkinter callback
+
+    # workaround for Exception in Tkinter callback:
     fig.canvas.start_event_loop(sys.float_info.min)
 
     fig.savefig(f"{run}{criteria}.png", bbox_inches='tight')
@@ -226,23 +242,23 @@ def get_ring(countermeasure, level='maks'):
             "95-persentil": 55.0
         },
         "sheltering_full": {
-            "maks": 2.5,
+            "maks": 2.,
             "95-persentil": 0.8
         },
         "sheltering_partial": {
-            "maks": 5.0,
+            "maks": 5.8,
             "95-persentil": 4.0
         },
         "evac": {
-            "maks": 1.75,
+            "maks": 1.95,
             "95-persentil": 0.5
         },
         "iodine_child": {
-            "maks": 9.0,
+            "maks": 9.3,
             "95-persentil": 4.0
         },
         "iodine_adult": {
-            "maks": 3.0,
+            "maks": 3.3,
             "95-persentil": 1.5
         }
     }
@@ -265,7 +281,7 @@ def plot_CM(run='', countermeasure=''):
         gdf_out = get_areas_above(run, countermeasure)
         gdf_out.plot(ax=ax, edgecolor="face", linewidth=1)
 
-        run = countermeasure + '_' + run.replace(".pkl.bz2", '')
+        run = countermeasure + '_' + run[3:].replace(".pkl.bz2", '')
     else:
         run = "tiltak_"+countermeasure
 
@@ -406,8 +422,29 @@ if __name__ == '__main__':
     # plot_run_boundary5km()
     # plot_run_boundary65km()
 
-    create_all(base5km)
-    create_all(base60km)
+    # create_all(base5km)
+    # create_all(base60km)
 
-    plot_CM(countermeasure='Matproduksjon')
+    # plot_CM(countermeasure='Matproduksjon')
     # plot_CM('iodine_child')
+
+
+    # Final:
+    #Evac
+    run = f'E:/{base5km}16800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
+    # plot_CM(run, 'evac')
+
+    # Full sheltering
+    run = f'E:/{base60km}4800_grid_toteffout_bitmp_Adults_Total__full.pkl.bz2'
+    # plot_CM(run, 'sheltering_partial')
+
+    # Iodine  1 year old
+    run = f'E:/{base60km}hyrod_bitmp_1year_Tota__full.pkl.bz2'
+    # plot_CM(run, 'iodine_child')
+
+    # Iodine  Adult
+    run = f'E:/{base5km}hyrod_bitmp_Adults_Tota__full.pkl.bz2'
+    # plot_CM(run, 'iodine_adult')
+
+    # reinsdyr uten ring:
+    plot("grotsund_arp_12h-maxgrid_depos_bitmp_I-131__full.pkl.bz2", 'reinsdyr')
